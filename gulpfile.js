11c8e4ef06810a517f6487ed394dbfad2d2eaa3c
Aiska Hendra <aiskahendra@gmail.com>
2016-03-11 21:31:36 +0700

    rename project to angular-slimscroll-aiska

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var closureCompiler = require('gulp-closure-compiler');
 
 gulp.task('default', function() {
-    return gulp.src('angular-slimScroll.js')
-        .pipe(closureCompiler('angular-slimScroll.min.js'))
+    return gulp.src('angular-slimscroll-aiska.js')
+        .pipe(closureCompiler('angular-slimscroll-aiska.min.js'))
         .pipe(gulp.dest(''));
 });
